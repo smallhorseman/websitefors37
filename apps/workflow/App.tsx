634fce0c9a2728bbfa33dc425122c587ef44f529
ownerofstudio37 <ceo@studio37.cc>
2025-11-04 20:19:03 -0600

    d

--- apps/workflow/App.tsx
+++ apps/workflow/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import { StatusBar } from 'expo-status-bar'
+import { View, Text } from 'react-native'
 import { AuthProvider } from './src/contexts/AuthContext'
 import { RootNavigator } from './src/navigation/RootNavigator'
 
 export default function App() {
+  console.log('🚀 Studio37 Workflow App Loading...')
+  
   return (
     <AuthProvider>
       <StatusBar style="light" />
